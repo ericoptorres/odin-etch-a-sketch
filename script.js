df2cfaa7b1fb94f67c5a727f53d80a52f97b2a01
erico <erico_pt@hotmail.com>
2023-03-29 13:41:22 -0300

    bad commit - created various functions

---
+++ script.js
@@ -0,0 +1,59 @@
+const container = document.querySelector('.container')
+let div = document.createElement('div')
+let col = document.createElement('div')
+let size = 16
+let boxSize = 500 / size
+let boxSizeAtribute 
+
+function createCol() {
+    let i = 1
+    col = document.createElement('div')
+    while (i <= size){
+        div = document.createElement('div')
+        div.classList.add('box')
+        boxSizeAtribute = `width: ${500/size}px; height: ${500/size}px; border: solid black 1px`
+        div.setAttribute('style', boxSizeAtribute)
+        col.appendChild(div)
+        i++
+    }
+    col.classList.add('col')
+    return col
+}
+
+function createGrid () {
+    let j = 1
+    while (j <= size){
+        container.appendChild(createCol())
+        j++
+    }
+}
+createGrid()
+
+
+let box = document.querySelectorAll('.box')
+function hoveringIn (box) {
+    box.forEach(elem => elem.addEventListener('mouseover', (e) => {
+        e.target.style.background = 'lightblue' 
+    }))
+}
+function hoveringOut (box){
+    box.forEach(elem => elem.addEventListener('mouseout', (e) => {
+        e.target.style.background = 'white' 
+    }))
+}
+
+hoveringIn(box)
+hoveringOut(box)
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {
+    size = parseInt(prompt("What's the new grid size: "))
+    container.textContent = ''
+    createGrid()
+    box = document.querySelectorAll('.box')
+    hoveringIn(box)
+    hoveringOut(box)
+})
+
+
+
